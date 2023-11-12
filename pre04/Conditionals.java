dca259d59a6f5e965ffb9a5f6ea8450d512df6a3
shaisabm <145130336+shaisabm@users.noreply.github.com>
2023-11-12 18:35:26 +0000

    initial commit

--- pre04/Conditionals.java
+++ pre04/Conditionals.java
@@ -6,14 +6,24 @@ public class Conditionals {
 
     Scanner age = new Scanner(System.in);
     System.out.print("Enter your age: ");
-    int yourAge = age.nextInt();
+    int yourAge = age.nextInt(); // Asking for your age
 
     if (yourAge < 18){
-      System.out.print("You are a minor");
+      System.out.println("You are a minor"); 
     }
     else{
-      System.out.print("You are an adult");
+      System.out.println("You are an adult ");
     } 
+    
+    if (yourAge %2 == 0){
+      System.out.println(yourAge+" is an even number");
+    }
+      
+    else{
+      System.out.println(yourAge+" is an odd number");
+    }
+    
+      
     
 
    
