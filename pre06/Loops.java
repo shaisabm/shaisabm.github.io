ccaf01bee270c322edb2d8159dd0d1e1b33bc3a6
shaisabm <145130336+shaisabm@users.noreply.github.com>
2023-11-12 20:24:11 +0000

    initial commit

---
+++ pre06/Loops.java
@@ -0,0 +1,34 @@
+public class Loops {
+
+  public static void main(String[] args){
+    
+    int n = 0;
+    int counting = 0; 
+
+    while(n < 21){  // Loop until is n < 21
+      System.out.println("Count "+counting);
+      counting++;  // Counting + 1 each time it loops
+      n++;         // n + 1 each time it loops
+    }
+
+            
+  int countingFrom20= 21;
+  int k = 0;
+
+  for(k = 0; k< 21; k++, countingFrom20++){   // k is 0, loop until k < 21, add 1 to k each time it loops, add 1 to 
+    System.out.println("Count "+countingFrom20); //countingFrom20 each time it loops
+    
+   
+
+    
+  }
+
+
+  
+
+
+    
+  }
+
+  
+}
