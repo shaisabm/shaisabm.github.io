81905fd5123894d8bc611e67693616d737ad93e8
shaisabm <145130336+shaisabm@users.noreply.github.com>
2023-12-06 16:03:42 +0000

    Initial commit

--- Pre01/Hello.java
+++ Pre01/Hello.java
@@ -5,5 +5,5 @@ public class Hello {
     public static void main(String[] args){
       String hello = "world!"; // "hello" is variable which contain the word "world!"
         System.out.println("Hello "+hello); // print the word Hello + "world!"
-    }
+    } // 
 }
